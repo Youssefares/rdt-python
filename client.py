fa9bdabebf7540129397357f28c88ed63409c0e3
Youssefares <Youssefares@ymail.com>
2018-04-19 00:24:16 +0200

    Modify client and server pre stop-and-wait
    Description: Client requests file with timeout for response from server. Server handles clients in child processes with the big TODO being handling the syncing issues fom using a single socket in parent process.

--- client.py
+++ client.py
@@ -3,6 +3,9 @@
 # Imports
 import socket
 import sys
+from threading import Thread, Event
+import select
+
 from config import client_config
 from Packet import Packet
 
@@ -21,16 +24,32 @@ SERVER_IP, SERVER_PORT, CLIENT_PORT, FILE, RCV_WINDOW_SIZE = client_config(
     CONFIG_FILE)
 
 
-MSG = FILE
-SPLIT_MSG = [MSG[i:i+PACKET_LEN] for i in range(0, len(MSG), PACKET_LEN)]
+REPLY_EVENT = Event()
+
+def send_file_name(s_client):
+  """ sends filename to se"""
+  msg = FILE
+  # Sending file name packet
+  packet = Packet(check_sum=1, seq_num=1, data=msg)
+  s_client.sendto(packet.bytes(), (SERVER_IP, SERVER_PORT))
+  print("Sent:", packet.bytes())
+
+  # Receive with timeout 5 seconds
+  ready = select.select([s_client], [], [], 5)
+  if ready[0]:
+    packet, addr = s_client.recvfrom(512) #Buffer_size = 512
+    if addr == (SERVER_IP, SERVER_PORT):
+      print("Reply received from: ", addr)
+      # Broadcast reply_event to stop sending requests
+      REPLY_EVENT.set()
+      #TODO: keep receiving and ack'ing according to packet length
 
 # Creating client socket
 S_CLIENT = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 S_CLIENT.bind(('localhost', CLIENT_PORT))
 
-# Sending Packets
-for i, msg in enumerate(SPLIT_MSG):
-  packet = Packet(check_sum=1, seq_num=i, data=msg)
-  S_CLIENT.sendto(packet.bytes(), (SERVER_IP, SERVER_PORT))
-  print("Sent:", packet.bytes())
-
+# Keep sending file name, if no reply from server within 5 secs
+while not REPLY_EVENT.is_set():
+  SEND_THREAD = Thread(target=send_file_name, args=[S_CLIENT])
+  SEND_THREAD.start()
+  SEND_THREAD.join(timeout=5)
--- server.py
+++ server.py
@@ -1,20 +1,45 @@
 """server"""
 
+# Imports
 import socket
 import sys
 from config import server_config
+from Packet import Packet
+from multiprocessing import Process
 
+# Constants
 CONFIG_FILE = "inputs/server.in"
+PACKET_LEN = 50
+
+# CMD args
 if len(sys.argv) > 2:
   raise Exception('Usage: server.py input_file.in')
 elif len(sys.argv) == 2:
   CONFIG_FILE = sys.argv[1]
 
+# More constants
 SERVER_PORT, SND_WINDOW_SIZE, RND_SEED, LOSS_PROP = server_config(CONFIG_FILE)
 
+# Creating server socket
 S_SERVER = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 S_SERVER.bind(('localhost', SERVER_PORT))
 
+
+def send_file(file_name, addr):
+  """sends file content for file with specified file_name to client"""
+  msg = open(file_name).read()
+  split_msg = [msg[i:i+PACKET_LEN] for i in range(0, len(msg), PACKET_LEN)]
+  # Sending Packets
+  for i, msg in enumerate(split_msg):
+    packet = Packet(check_sum=1, seq_num=i, data=msg)
+    S_SERVER.sendto(packet.bytes(), addr)
+    print("Replied with packet #%i:" % i, packet.bytes(), "To client: ", addr)
+
+
 while True:
-  file_name, addr = S_SERVER.recvfrom(512) #Buffer_size
-  print("Message: ", file_name, "Addr: ", addr)
+  PACKET, ADDR = S_SERVER.recvfrom(512) #Buffer_size = 512
+  FILE_NAME = PACKET[3:].decode('utf-8')
+  print("Filename requested by client: ", FILE_NAME, "Addr: ", ADDR)
+  P = Process(target=send_file, args=[FILE_NAME, ADDR])
+  P.start()
+  P.join()
