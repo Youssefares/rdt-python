5972730d788037b4dabbc4bbc17c7ddb31969e51
Youssefares <Youssefares@ymail.com>
2018-05-07 00:03:33 +0200

    Made client.py easier to run

--- rdt_python/client.py
+++ rdt_python/client.py
@@ -3,6 +3,7 @@ import sys
 
 from GoBackN.GoBackNClient import GoBackNClient
 from SelectiveRepeat.SelectiveRepeatClient import SelectiveRepeatClient
+from StopAndWaitClient import main_receive_loop
 from config import client_config
 
 
@@ -20,12 +21,15 @@ client_class_dict = {
 if __name__ == '__main__':
     client_protocol = sys.argv[1]
     dest = sys.argv[2]
-
-    # get class constructor from first cmd line arg
-    client = client_class_dict[client_protocol]
-
+    
     probability = 0.3
     seed_num = 3000
+    if client_protocol == 'sw':
+      main_receive_loop(file_name, probability, seed_num)
+    else:
+      # get class constructor from first cmd line arg
+      client = client_class_dict[client_protocol]
 
-    logging.basicConfig(level=logging.DEBUG)
-    client(('localhost', CLIENT_PORT), probability, seed_num).request_file((SERVER_IP, SERVER_PORT), FILE, dest)
+
+      logging.basicConfig(level=logging.DEBUG)
+      client(('localhost', CLIENT_PORT), probability, seed_num).request_file((SERVER_IP, SERVER_PORT), FILE, dest)
